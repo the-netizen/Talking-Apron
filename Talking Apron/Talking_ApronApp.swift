f02db58d5b48850adf238879d3a044e6d1ae1e90
the-netizen <khanaima020815@gmail.com>
2026-01-07 07:38:29 +0300

    Initial Commit

---
+++ Talking Apron/Talking_ApronApp.swift
@@ -0,0 +1,32 @@
+//
+//  Talking_ApronApp.swift
+//  Talking Apron
+//
+//  Created by Naima Khan on 07/01/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Talking_ApronApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
